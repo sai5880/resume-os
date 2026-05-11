5e4b84552a1c5a872c2c8ecf025e4ca7111c51f4
sai5880 <dev.sai5880@gmail.com>
2026-05-11 21:22:13 +0530

    dep2

--- server/assets/pdf.worker.min-DsEtpD-e.js
+++
@@ -1,4 +0,0 @@
-const pdf_worker_min = "./assets/pdf.worker.min-iDqQPrd3.mjs";
-export {
-  pdf_worker_min as default
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-Ch3UOo6f.js";
-import "node:events";
-export {
-  w as default
-};
